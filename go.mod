271fa56870d64ba3169d3d5bcfd83cb71f898be7
Dimitri Acosta <dimitri316@gmail.com>
2021-05-09 01:26:44 -0600

    Create a greetings package

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module example.com/hello
 
 go 1.16
 
-require github.com/dimitriacosta/stringutil v0.0.0-20191126061809-1cf810c010ab
+replace example.com/greetings => ../greetings
+
+require (
+	example.com/greetings v0.0.0-00010101000000-000000000000
+	example.com/stringutil v0.0.0-00010101000000-000000000000
+)
+
+replace example.com/stringutil => ../stringutil
--- hello.go
+++ hello.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
-	"github.com/dimitriacosta/stringutil"
+	"log"
+
+	"example.com/greetings"
+	"example.com/stringutil"
 )
 
 func main() {
-	fmt.Println("Normal: Hello world!")
-	fmt.Println("Reversed:", stringutil.Reverse("Hello world!"))
+	m := "Hello world"
+	// Show a simple hello world message.
+	fmt.Println(m)
+	// Now reverse the hello world message.
+	fmt.Println(stringutil.Reverse(m))
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Dimitri", "Alejandra", "Anya"}
+
+	// Request a greeting messages for the names.
+	message, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
